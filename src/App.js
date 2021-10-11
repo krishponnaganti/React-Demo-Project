d6bc4b52930789b89777276a6671ce14d0a194d4
RK <krishponnaganti@gmail.com>
2021-10-11 15:23:09 +0530

    ga changes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { useHistory,useLocation } from "react-router";
 import ReactGA from "react-ga";
+import { useEffect } from "react";
 function App() {
 //  ReactGA.initialize("G-0YFP5Y996K");
-  ReactGA.initialize("UA-209931511-1");
+  useEffect(()=>{
+    ReactGA.initialize("UA-209931511-1");
+    ReactGA.pageview("/");
+  },[])
   return (
     <>
       <div className="col-md-12">
